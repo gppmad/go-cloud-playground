60137383117c10c60ef236c6c9617a8d7866c6aa
Giuseppe Maddiona <giuseppe.maddiona@gmail.com>
2024-03-09 19:51:59 +0100

    add a simple function with a simple test

--- db.go
+++ db.go
@@ -11,3 +11,7 @@ func anotherHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("printed Another Handler")
 	fmt.Fprint(w, "Another Handler!")
 }
+
+func simpleSum(a int, b int) int {
+	return a + b
+}
