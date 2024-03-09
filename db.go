824b8b7059b693a0af499ba83d974f2f9ad67705
Giuseppe Maddiona <giuseppe.maddiona@gmail.com>
2024-03-09 19:33:16 +0100

    I changed idea, I want to transform this project.
    I will create a simple suite of command that read and write from a SQLite db.

---
+++ db.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// TODO: Transform this function in something that fetch data from db.
+func anotherHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("printed Another Handler")
+	fmt.Fprint(w, "Another Handler!")
+}
