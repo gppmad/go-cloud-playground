c83622acb100859bf3a00745190e761fcd2dd16e
Giuseppe Maddiona <giuseppe.maddiona@gmail.com>
2024-03-27 17:32:55 +0100

    removing testing example

--- db.go
+++ db.go
@@ -6,12 +6,9 @@ import (
 	"net/http"
 )
 
-// TODO: Transform this function in something that fetch data from db.
 func anotherHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("printed Another Handler")
 	fmt.Fprint(w, "Another Handler!")
 }
 
-func simpleSum(a int, b int) int {
-	return a + b
-}
+// SELECT DB Version
