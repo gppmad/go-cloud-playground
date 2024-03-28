47f4d23aff39f52329bc9e6c66c334e895afa68a
Giuseppe Maddiona <giuseppe.maddiona@gmail.com>
2024-03-28 13:57:16 +0100

    cleaning the project and starting the db integration

--- db.go
+++ db.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"log"
-	"net/http"
 )
 
-func anotherHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("printed Another Handler")
-	fmt.Fprint(w, "Another Handler!")
-}
+func getDBVersion() {
 
-// SELECT DB Version
+	urlExample := "postgres://golang:golangdb@localhost:5432/golangdatabase"
+	db, err := sql.Open("postgres", urlExample)
+	if err != nil {
+		fmt.Println("Connection error with the DB")
+		return
+	}
+	defer db.Close()
+
+	// Perform a select query
+	rows, err := db.Query("SELECT version();")
+	if err != nil {
+		fmt.Println("Error executing query:", err)
+		return
+	}
+	defer rows.Close()
+
+	// Print Result
+	for rows.Next() {
+		var dbversion string
+
+		err := rows.Scan(&dbversion)
+		if err != nil {
+			fmt.Println("Error scanning row:", err)
+			return
+		}
+
+		fmt.Printf("DB Version: %s\n", dbversion)
+	}
+
+	// Check for errors during row iteration
+	if err := rows.Err(); err != nil {
+		fmt.Println("Error iterating rows:", err)
+		return
+	}
+}
--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("printed Hello World")
-	fmt.Fprint(w, "Hello, World!")
-
-}
-
 func main() {
-	http.HandleFunc("/hello", helloHandler)
-	http.HandleFunc("/another-handler", anotherHandler)
-	log.Println("Server starting on port 10000")
-	err := http.ListenAndServe(":10000", nil)
-	if err != nil {
-		log.Fatal("Error starting the web server: ", err)
-	}
-
+	getDBVersion()
 }
