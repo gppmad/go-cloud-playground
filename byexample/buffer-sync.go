8f284d9eda4c67effed038939f3cdd9e011d5f1c
giuseppe.maddiona <giuseppe.maddiona@momox.biz>
2024-09-23 13:40:55 +0200

    buffer-sync.go

---
+++ byexample/buffer-sync.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(done chan bool) {
+	fmt.Println("Doing something")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	done <- true
+}
+
+/* func main() {
+	doneChannel := make(chan bool, 1)
+	go worker(doneChannel)
+	//<-doneChannel
+
+} */
